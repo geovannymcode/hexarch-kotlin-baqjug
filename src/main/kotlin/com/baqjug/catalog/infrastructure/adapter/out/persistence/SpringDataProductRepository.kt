7e053c3577631ca1e36d59ce6e977bca01efedb7
Geovanny Mendoza <me@geovannycode.com>
2026-06-01 12:00:00 -0500

    sesion-3: adaptadores REST y JPA + app corriendo

--- src/main/kotlin/com/baqjug/catalog/infrastructure/adapter/out/persistence/SpringDataProductRepository.kt
+++
@@ -1,5 +0,0 @@
-package com.baqjug.catalog.infrastructure.adapter.out.persistence
-
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface SpringDataProductRepository : JpaRepository<ProductJpaEntity, String>
